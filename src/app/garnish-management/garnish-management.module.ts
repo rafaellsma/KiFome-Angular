f1a2aa0ef567254d761e92490283d8fbfb46b467
Rafael <rafaellsma@gmail.com>
2018-02-05 18:38:31 -0300

    Adiciona servico de acompanhamento

--- src/app/garnish-management/garnish-management.module.ts
+++ src/app/garnish-management/garnish-management.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListGarnishesComponent } from './list-garnishes/list-garnishes.component';
 import { MaterialModule } from '../material/material.module';
+import { GarnishService } from './garnish.service';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule
   ],
-  declarations: [ListGarnishesComponent]
+  declarations: [ListGarnishesComponent],
+  providers: [
+    GarnishService
+  ]
 })
 export class GarnishManagementModule { }
