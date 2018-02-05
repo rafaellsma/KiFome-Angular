06f74930c73a6fa274daff3ef0fc4a12ffc466ba
Rafael <rafaellsma@gmail.com>
2018-02-05 18:29:10 -0300

    Adiciona o component  lista de acompanhamentos

--- src/app/garnish-management/garnish-management.module.ts
+++ src/app/garnish-management/garnish-management.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListGarnishesComponent } from './list-garnishes/list-garnishes.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ListGarnishesComponent]
 })
 export class GarnishManagementModule { }
---
+++ src/app/garnish-management/list-garnishes/list-garnishes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-garnishes',
+  templateUrl: './list-garnishes.component.html',
+  styleUrls: ['./list-garnishes.component.css']
+})
+export class ListGarnishesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
