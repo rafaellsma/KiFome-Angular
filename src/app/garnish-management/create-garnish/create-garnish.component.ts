576940a7aff266b241a288a4904addcc384206c5
Rafael <rafaellsma@gmail.com>
2018-02-06 16:38:03 -0300

    Adiciona componente para criar acompanhamentos'

---
+++ src/app/garnish-management/create-garnish/create-garnish.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-garnish',
+  templateUrl: './create-garnish.component.html',
+  styleUrls: ['./create-garnish.component.css']
+})
+export class CreateGarnishComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/garnish-management/garnish-management.module.ts
+++ src/app/garnish-management/garnish-management.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ListGarnishesComponent } from './list-garnishes/list-garnishes.component';
 import { MaterialModule } from '../material/material.module';
 import { GarnishService } from './garnish.service';
+import { CreateGarnishComponent } from './create-garnish/create-garnish.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule
   ],
-  declarations: [ListGarnishesComponent],
+  declarations: [ListGarnishesComponent, CreateGarnishComponent],
   providers: [
     GarnishService
   ]
