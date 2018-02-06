40a6ba67ba77acf767002033dac133fb9439bea1
Rafael <rafaellsma@gmail.com>
2018-02-06 16:36:42 -0300

    Termina a view de lista de acompanhamentos

--- src/app/garnish-management/garnish.service.ts
+++ src/app/garnish-management/garnish.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { GarnishDTO } from './models/garnish.dto';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class GarnishService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
+  getAllGarnishes(): Observable<GarnishDTO[]> {
+    return this.httpClient.get<GarnishDTO[]>('http://localhost:49849/api/garnishes');
+  }
 }
+
--- src/app/garnish-management/list-garnishes/list-garnishes.component.ts
+++ src/app/garnish-management/list-garnishes/list-garnishes.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { GarnishService } from '../garnish.service';
+import { GarnishDTO } from '../models/garnish.dto';
+import { Observable } from 'rxjs/Observable';
+import { MatTableDataSource } from '@angular/material';
+import { DataSource } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-list-garnishes',
@@ -7,9 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListGarnishesComponent implements OnInit {
 
-  constructor() { }
+  garnishesDataSource = new GarnishDataSource(this.garnishService);
+  garnishesAttributes = ['Name', 'Description'];
+
+  constructor(private garnishService: GarnishService) {}
 
   ngOnInit() {
   }
 
+}
+
+class GarnishDataSource extends DataSource<any> {
+   constructor(private garnishService: GarnishService){
+     super();
+   }
+
+   connect(): Observable<GarnishDTO[]> {
+     return this.garnishService.getAllGarnishes();
+   }
+
+   disconnect() {}
 }
