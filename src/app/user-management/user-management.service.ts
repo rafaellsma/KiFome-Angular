07655cde8fdad224dd5c59246cc63ce5a21457b4
JoaoPauloLins <jplo@cin.ufpe.br>
2018-02-07 17:06:25 -0200

    Correção de conflito

--- src/app/user-management/user-management.service.ts
+++ src/app/user-management/user-management.service.ts
@@ -9,14 +9,8 @@ import { User } from '../shared/models/user';
 export class UserManagementService {
 
     constructor(private http: HttpClient) {}
-<<<<<<< HEAD
-  
-    authenticate(authentication: Authentication): Observable<User> {
-        return this.http.post<User>("http://localhost:49849/api/user", authentication);
-=======
 
     authenticate(authentication: Authentication): Observable<User> {
-        return this.http.post<User>('', authentication);
->>>>>>> master
+        return this.http.post<User>('http://localhost:49849/api/user', authentication);
     }
 }
